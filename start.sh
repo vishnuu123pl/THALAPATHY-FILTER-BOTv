3d8caef81dbd489e65642e55660ed2cfd14aab55
Mujahith123 <127096028+Mujahith123@users.noreply.github.com>
2023-12-20 22:18:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/vishnuu123pl/THALAPATHY-FILTER-BOTv.git /THALAPATHY-FILTER-BOTv 
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOTv 
-fi
-cd /THALAPATHY-FILTER-BOTv 
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+#thanks for watching 
